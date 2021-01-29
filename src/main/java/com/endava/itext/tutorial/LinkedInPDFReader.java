0b1ea4d7b5deeb5672e1881689d20b45e09de844
Bogdan Benga <bogdan.benga@endava.com>
2021-01-29 17:31:32 +0200

    Switch to PDFBox

--- src/main/java/com/endava/itext/tutorial/LinkedInPDFReader.java
+++ src/main/java/com/endava/itext/tutorial/LinkedInPDFReader.java
@@ -1,91 +1,50 @@
 package com.endava.itext.tutorial;
 
-import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.parser.*;
-
-import java.io.IOException;
-
 public class LinkedInPDFReader {
 
-    PdfReader pdfReader;
-
-    public LinkedInPDFReader(String filePath) throws IOException {
-        this.pdfReader = new PdfReader(filePath);
-    }
-
-    public String getCurrentEmployer() throws IOException {
-
-        Rectangle left = new Rectangle(0, 0, 306, 792);
-        Rectangle right = new Rectangle(306, 0, 612, 792);
-        RenderFilter leftFilter = new RegionTextRenderFilter(left);
-        RenderFilter rightFilter = new RegionTextRenderFilter(right);
-        TextExtractionStrategy strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), leftFilter);
-        String original_content = PdfTextExtractor.getTextFromPage(pdfReader, 1, strategy);
-        original_content += " \n-----------------------------------------------------------\n";
-        System.out.print(original_content);
-        strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), rightFilter);
-        original_content += PdfTextExtractor.getTextFromPage(pdfReader, 1, strategy);
-        System.out.print(original_content);
-        return null;
-    }
-
-    public <ITextExtractionStrategy> String getCoreSkills() {
-//        Rectangle rect = null;
-//        RenderFilter[] filter = {new RegionTextRenderFilter(rect)};
-//        ITextExtractionStrategy strategy;
-//        StringBuilder sb = new StringBuilder();
-//        for (int i = 1; i <= pdfReader.getNumberOfPages(); i++) {
-//            strategy = (ITextExtractionStrategy) new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
-//            sb.AppendLine(PdfTextExtractor.GetTextFromPage(pdfReader, i, strategy));
+//    PdfReader pdfReader;
+//
+//    public LinkedInPDFReader(String filePath) throws IOException {
+//        this.pdfReader = new PdfReader(filePath);
+//    }
+//
+//    public String getCurrentEmployer() throws IOException {
+//
+//        return null;
+//    }
+//
+//    public <ITextExtractionStrategy> String getCoreSkills() {
+////
+//        return null;
+//    }
+//
+//    public <ITextExtractionStrategy> String getCurrentJobTitle() {
+////
+//        return null;
+//    }
+//
+//    public Integer getTotalYearsOfExperience() {
+////
+//        return null;
+//    }
+//
+//
+//    public void readPdf() throws IOException {
+//
+//        //Get the number of pages in pdf.
+//        int pages = pdfReader.getNumberOfPages();
+//
+//        for (int i = 0; i < pdfReader.getXrefSize(); i++) {
+//            PdfObject pdfobj = pdfReader.getPdfObject(i);
+//            if (pdfobj == null || !pdfobj.isStream()) {
+//                continue;
+//            }
+//            PdfStream stream = (PdfStream) pdfobj;
+//            PdfObject pdfsubtype = stream.get(PdfName.SUBTYPE);
+//            System.out.println(pdfsubtype.toString());
 //        }
-        return null;
-    }
-
-    public <ITextExtractionStrategy> String getCurrentJobTitle() {
-//        Rectangle rect = null;
-//        RenderFilter[] filter = {new RegionTextRenderFilter(rect)};
-//        ITextExtractionStrategy strategy;
-//        StringBuilder sb = new StringBuilder();
-//        for (int i = 1; i <= pdfReader.getNumberOfPages(); i++) {
-//            strategy = (ITextExtractionStrategy) new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
-//            sb.AppendLine(PdfTextExtractor.GetTextFromPage(pdfReader, i, strategy));
-//        }
-        return null;
-    }
-
-    public Integer getTotalYearsOfExperience() {
-//        Rectangle rect = null;
-//        RenderFilter[] filter = {new RegionTextRenderFilter(rect)};
-//        ITextExtractionStrategy strategy;
-//        StringBuilder sb = new StringBuilder();
-//        for (int i = 1; i <= pdfReader.getNumberOfPages(); i++) {
-//            strategy = (ITextExtractionStrategy) new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
-//            sb.AppendLine(PdfTextExtractor.GetTextFromPage(pdfReader, i, strategy));
-//        }
-        return null;
-    }
-
-
-    public void readPdf() throws IOException {
-
-        //Get the number of pages in pdf.
-        int pages = pdfReader.getNumberOfPages();
-
-        //Iterate the pdf through pages.
-        for (int i = 1; i <= pages; i++) {
-            //Extract the page content using PdfTextExtractor.
-            String pageContent =
-                    PdfTextExtractor.getTextFromPage(pdfReader, i);
-
-            //Print the page content on console.
-            System.out.println("Content on Page "
-                    + i + ": " + pageContent);
-        }
-
-        //Close the PdfReader.
-        pdfReader.close();
-    }
+//        pdfReader.close();
+//    }
 
 
 }
--- src/main/java/com/endava/itext/tutorial/MainApp.java
+++ src/main/java/com/endava/itext/tutorial/MainApp.java
@@ -1,29 +1,28 @@
 package com.endava.itext.tutorial;
 
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDResources;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class MainApp {
 
-    public static void main(String[] args) {
-        String currentEmployer;
-        String coreSkills;
-        String currentJobTitle;
-        Integer totalYearsOfExperience;
+    public static void main(String[] args) throws IOException {
+        PDDocument document = null;
+        File file = new File(MainApp.class.getClassLoader().getResource("pdfs/BogdanBengaProfile.pdf").getPath());
+        document = PDDocument.load(file);
+        int pageNum = 0;
+        for( PDPage page : document.getPages() ){
+            for (COSName cosName : page.getResources().getXObjectNames()){
+                System.out.println(page.getResources().getXObject(cosName).toString());
 
-        try {
-            LinkedInPDFReader pdfReader = new LinkedInPDFReader("/pdfs/BogdanBengaProfile.pdf");
-//            pdfReader.readPdf();
-
-            //set current Employer
-            currentEmployer = pdfReader.getCurrentEmployer();
-            //set core Skills
-            coreSkills = pdfReader.getCoreSkills();
-            //set currentJob Title
-            currentJobTitle = pdfReader.getCurrentJobTitle();
-            //set total years of experience
-            totalYearsOfExperience = pdfReader.getTotalYearsOfExperience();
-        } catch (IOException e) {
-            e.printStackTrace();
+            }
         }
 
 
