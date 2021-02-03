67169658f890c58adcda2245a8319d78f678429f
Bogdan Augustin Benga <bogdan.benga@endava.com>
2021-02-03 15:32:51 +0200

    Refactored code and added guidlines

--- src/main/java/com/endava/itext/tutorial/LinkedInPDFReader.java
+++ src/main/java/com/endava/itext/tutorial/LinkedInPDFReader.java
@@ -25,39 +25,63 @@ public class LinkedInPDFReader {
     }
 
     public String getCurrentEmployer() {
-        return null;
+        if (isLinkedInPdf()) {
+            // Get the resultant text after applying the custom filter
+            String actualText = getActualText(1, false);
+            //in string you can split by \\nl and get it into an array
+            // extract the string after "at" or after "la" from the second line
+
+            return actualText;
+        } else {
+            return null;
+        }
     }
 
     public String getTopSkills() {
 
         if (isLinkedInPdf()) {
-
-            float pageWidth = pdfDocument.getFirstPage().getPageSize().getWidth();
-            float pageHeight = pdfDocument.getFirstPage().getPageSize().getHeight();
-
-            float left = pdfDocument.getFirstPage().getPageSize().getLeft();
-            float bottom = pdfDocument.getFirstPage().getPageSize().getBottom();
-
-            Rectangle summaryRectangle = new Rectangle(left, bottom, pageWidth / 3, pageHeight);
-            TextRegionEventFilter regionEventFilter = new TextRegionEventFilter(summaryRectangle);
-            FilteredEventListener listener = new FilteredEventListener();
-
-
-            // Create a text extraction renderer
-            LocationTextExtractionStrategy extractionStrategy = listener
-                    .attachEventListener(new LocationTextExtractionStrategy(), regionEventFilter);
-
-            // Note: If you want to re-use the PdfCanvasProcessor, you must call PdfCanvasProcessor.reset()
-            PdfCanvasProcessor parser = new PdfCanvasProcessor(listener);
-            parser.processPageContent(pdfDocument.getFirstPage());
-
             // Get the resultant text after applying the custom filter
-            String actualText = extractionStrategy.getResultantText();
+            String actualText = getActualText(1, true);
+
+            //check if Top Skills and Languages exist in text, if true apply this one
             String topSkills = CustomStringUtils.getBetweenStrings(actualText, "Top Skills\n", "\nLanguages");
+
+            //else check if "Aptitudini Principale" and "Languages" exist then update the call of topsSkills assignment
             return topSkills.replace("\n", ", ");
         } else {
             return null;
         }
+    }
+
+    private String getActualText(int pageNo, boolean isSummarySide) {
+        float pageWidth = pdfDocument.getPage(pageNo).getPageSize().getWidth();
+        float pageHeight = pdfDocument.getPage(pageNo).getPageSize().getHeight();
+
+        float left = pdfDocument.getPage(pageNo).getPageSize().getLeft();
+        float bottom = pdfDocument.getPage(pageNo).getPageSize().getBottom();
+
+        Rectangle summaryRectangle;
+
+        if (isSummarySide) {
+            summaryRectangle = new Rectangle(left, bottom, pageWidth / 3, pageHeight);
+        } else {
+            summaryRectangle = new Rectangle(pageHeight / 3, bottom, pageWidth - pageWidth / 3, pageHeight);
+        }
+
+        TextRegionEventFilter regionEventFilter = new TextRegionEventFilter(summaryRectangle);
+        FilteredEventListener listener = new FilteredEventListener();
+
+
+        // Create a text extraction renderer
+        LocationTextExtractionStrategy extractionStrategy = listener
+                .attachEventListener(new LocationTextExtractionStrategy(), regionEventFilter);
+
+        // Note: If you want to re-use the PdfCanvasProcessor, you must call PdfCanvasProcessor.reset()
+        PdfCanvasProcessor parser = new PdfCanvasProcessor(listener);
+        parser.processPageContent(pdfDocument.getPage(pageNo));
+
+        // Get the resultant text after applying the custom filter
+        return extractionStrategy.getResultantText();
     }
 
     private boolean isLinkedInPdf() {
@@ -65,7 +89,7 @@ public class LinkedInPDFReader {
         return pdfAuthor.equals(LINKEDIN_AUTHOR_NAME);
     }
 
-    public <ITextExtractionStrategy> String getCurrentJobTitle() {
+    public String getCurrentJobTitle() {
         return null;
     }
 
@@ -78,8 +102,6 @@ public class LinkedInPDFReader {
 
 
     }
-
-
 
 
 }
--- src/main/java/com/endava/itext/tutorial/MainApp.java
+++ src/main/java/com/endava/itext/tutorial/MainApp.java
@@ -11,6 +11,9 @@ public class MainApp {
             LinkedInPDFReader linkedInPDFReader = new LinkedInPDFReader("/pdfs/BogdanBengaProfile.pdf");
             String topSkills = linkedInPDFReader.getTopSkills();
             System.out.println(topSkills);
+
+            String currentEmployer = linkedInPDFReader.getCurrentEmployer();
+            System.out.println(currentEmployer);
         } catch (IOException e) {
             e.printStackTrace();
         }
